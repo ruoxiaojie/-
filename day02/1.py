8ad6084a66ca27b942dcee26823ddfb7d6007b5e
LJack <475030894@qq.com>
2017-10-23 18:43:38 +0800

    1023

---
+++ day02/1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#Author:xiaojie
+# -*- coding:utf-8 -*-
+
+# import re
+# time1 = 20171020233000
+# time2 = 20171020233500
+# with open(r'I:\reptile\day02\1.txt',mode='r',encoding='utf-8') as f,open('2.txt',mode='a+',encoding='utf-8') as f1:
+#     for line in f:
+#         a = re.findall('(201\d+),', line)
+#         b = int(a[0])
+#         # print(b)
+#         if b < time1:
+#            f1.write(line)
+#         if b > time2:
+#             f1.write(line)
+#     f1.close()
+
+
+
+
+
+a = 'qwaertyayttut'
+print(a.split('a'))
+
+
+
+
+
+
--- day02/2.py
+++ day02/2.py
@@ -2,3 +2,7 @@
 #Author:xiaojie
 # -*- coding:utf-8 -*-
 
+
+
+a = ['20171020231502']
+print(a[0])
